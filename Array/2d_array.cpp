5d3e3640dc65ce9909d188e048a2149dc15aa8a9
pranav <hyprocus@gmail.com>
2021-10-01 15:28:17 +0530

    added addition of matrix,2d Array, array traversal programs

---
+++ Array/2d_array.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+        
+
+
+
+        
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        
+        cout<<"\n";
+        
+    }    
+}
---
+++ Array/array_traversal_using_for_loop.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[]={4,5,6,7,8};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
