1119c670d5c609800005c2fcac9da9faf42000a5
Paridhi Jain <18ec.paridhijain@gmail.com>
2021-10-02 23:29:10 +0530

    Create Largest number at least twice of others.cpp

---
+++ C++ Programs/Largest number at least twice of others.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int maxIndex=0;
+        int max=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] > max)
+                {max = nums[i];
+                maxIndex = i;}
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != max && max<2*nums[i])
+                return -1;
+        }
+        
+        return maxIndex;
+        
+    }
+};
