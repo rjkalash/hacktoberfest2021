96c57c537c8eff8e3a5e4b80345398f6308ab669
kirat pal singh lamba <32259407+skirat@users.noreply.github.com>
2021-10-03 14:44:31 +0530

    Create add-remove.py

---
+++ Python/add-remove.py
@@ -0,0 +1,16 @@
+# Declare a fruit list
+fruits = ["Mango","Orange","Guava","Banana"]
+
+# Insert an item in the 2nd position
+fruits.insert(1, "Grape")
+
+# Displaying list after inserting
+print("The fruit list after insert:")
+print(fruits)
+ 
+# Remove an item
+fruits.remove("Guava")
+
+# Print the list after delete
+print("The fruit list after delete:")
+print(fruits)
