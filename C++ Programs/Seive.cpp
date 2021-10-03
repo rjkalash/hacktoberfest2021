774c197797e84dbdedb1468edae1822c01f830c0
Prasham Bhagwat <70129413+Datguyprasham@users.noreply.github.com>
2021-10-03 10:01:04 +0530

    Added functions related to prime numbers
    A simple method to check a if a number is prime or not , another function to generate prime factors.

--- C++ Programs/Seive.cpp
+++ C++ Programs/Seive.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int n;
+
+// Check if n is a prime in O(sqrt(n))
+bool prime_check(int n) { 
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+//Sieve of eratosthenes
 vector<bool> is_prime(n+1, true);
 is_prime[0] = is_prime[1] = false;
 void seive() {
@@ -12,6 +23,22 @@ void seive() {
         }
     }
 }
+
+//prime factorization in O(log n)
+void prime_fact(){ 
+    for(int i=1;i<=n;i++)
+        arr[i]=-1;
+
+    for(int i=2;i<=n;i++){
+        if(arr[i]==-1){
+            for(int j=i;j<=n;j+=i){
+                if(arr[j]==-1)
+                    arr[j]=i;
+            }
+        }
+    }
+}
+
 
 int main() {
     cin>>n;
