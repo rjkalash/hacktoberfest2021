470e4bb85bbde74ab5d0f4383ed41c8ea52d6319
Gourav <71971801+gouravchawla334@users.noreply.github.com>
2021-10-04 16:50:48 +0530

    Add files via upload

---
+++ C++ Programs/pattern/pattern01.cpp
@@ -0,0 +1,23 @@
+/*
+                        123
+                        123
+                        123
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 3; i++)
+    {
+        for (j = 1; j <= 3; j++)
+        {
+            cout << j;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
