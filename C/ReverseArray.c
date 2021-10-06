ed3517179fd6dbb8f053e22a88a5482c4098efe3
sahil-9287 <91861141+sahil-9287@users.noreply.github.com>
2021-10-06 13:48:01 +0530

    Update ReverseArray.c

--- C/ReverseArray.c
+++ C/ReverseArray.c
@@ -1,30 +1,40 @@
-#include<stdio.h>
-void rvereseArray(int arr[], int start, int end)
-{
-	int temp;
-	while (start < end)
-	{
-		temp = arr[start];
-		arr[start] = arr[end];
-		arr[end] = temp;
-		start++;
-		end--;
-	}
-}	
-void printArray(int arr[], int size)
-{
-int i;
-for (i=0; i < size; i++)
-	printf("%d ", arr[i]);
-printf("\n");
-}
+#include <stdio.h>
+ 
+
 int main()
 {
-	int arr[] = {1, 2, 3, 4, 5, 6};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	printArray(arr, n);
-	rvereseArray(arr, 0, n-1);
-	printf("Reversed array is \n");
-	printArray(arr, n);
-	return 0;
+    int arr[100], reverse[100];
+    int size, i, arrIndex, revIndex;
+
+   
+    printf("Enter size of the array: ");
+    scanf("%d", &size);
+
+    
+    printf("Enter elements in array: \n");
+    for(i=0; i<size; i++)
+    {
+    	printf("a[%d] = ",i);
+        scanf("%d", &arr[i]);
+    }
+
+    revIndex = 0;
+    arrIndex = size - 1;
+    while(arrIndex >= 0)
+    {
+       
+        reverse[revIndex] = arr[arrIndex];
+        
+        revIndex++;
+        arrIndex--;
+    }
+
+    
+    printf("\nReversed array : ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d ", reverse[i]);
+    }
+
+   
 }
