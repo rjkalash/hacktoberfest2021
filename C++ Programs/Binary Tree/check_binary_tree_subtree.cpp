9dc6ac39be902545531ed46753764abded05b4f6
Arnav2982 <78140728+Arnav2982@users.noreply.github.com>
2021-10-07 09:32:30 +0530

    Update check_binary_tree_subtree.cpp

--- C++ Programs/Binary Tree/check_binary_tree_subtree.cpp
+++ C++ Programs/Binary Tree/check_binary_tree_subtree.cpp
@@ -1,115 +1,74 @@
-// C++ program to check if binary tree
-// is subtree of another binary tree
 #include<bits/stdc++.h>
+
 using namespace std;
 
 /* A binary tree node has data,
-left child and right child */
+pointer to left child and
+a pointer to right child */
 class node
 {
 	public:
 	int data;
 	node* left;
 	node* right;
+	
+	/* Constructor that allocates
+	a new node with the given data
+	and NULL left and right pointers. */
+	node(int data)
+	{
+		this->data = data;
+		this->left = NULL;
+		this->right = NULL;
+	}
 };
 
-/* A utility function to check
-whether trees with roots as root1 and
-root2 are identical or not */
-bool areIdentical(node * root1, node *root2)
+int isBSTUtil(node* node, int min, int max);
+
+/* Returns true if the given
+tree is a binary search tree
+(efficient version). */
+int isBST(node* node)
 {
-	/* base cases */
-	if (root1 == NULL && root2 == NULL)
-		return true;
-
-	if (root1 == NULL || root2 == NULL)
-		return false;
-
-	/* Check if the data of both roots is
-	same and data of left and right
-	subtrees are also same */
-	return (root1->data == root2->data &&
-			areIdentical(root1->left, root2->left) &&
-			areIdentical(root1->right, root2->right) );
+	return(isBSTUtil(node, INT_MIN, INT_MAX));
 }
 
-
-/* This function returns true if S
-is a subtree of T, otherwise false */
-bool isSubtree(node *T, node *S)
+/* Returns true if the given
+tree is a BST and its values
+are >= min and <= max. */
+int isBSTUtil(node* node, int min, int max)
 {
-	/* base cases */
-	if (S == NULL)
-		return true;
-
-	if (T == NULL)
-		return false;
-
-	/* Check the tree with root as current node */
-	if (areIdentical(T, S))
-		return true;
-
-	/* If the tree with root as current
-	node doesn't match then try left
-	and right subtrees one by one */
-	return isSubtree(T->left, S) ||
-		isSubtree(T->right, S);
+	/* an empty tree is BST */
+	if (node==NULL)
+		return 1;
+			
+	/* false if this node violates
+	the min/max constraint */
+	if (node->data < min || node->data > max)
+		return 0;
+	
+	/* otherwise check the subtrees recursively,
+	tightening the min or max constraint */
+	return
+		isBSTUtil(node->left, min, node->data-1) && // Allow only distinct values
+		isBSTUtil(node->right, node->data+1, max); // Allow only distinct values
 }
 
-
-/* Helper function that allocates
-a new node with the given data
-and NULL left and right pointers. */
-node* newNode(int data)
-{
-	node* Node = new node();
-	Node->data = data;
-	Node->left = NULL;
-	Node->right = NULL;
-	return(Node);
-}
 
 /* Driver code*/
 int main()
 {
-	// TREE 1
-	/* Construct the following tree
-			26
-			/ \
-		10 3
-		/ \ \
-	4 6 3
-	\
-		30
-	*/
-	node *T = newNode(26);
-	T->right		 = newNode(3);
-	T->right->right = newNode(3);
-	T->left		 = newNode(10);
-	T->left->left	 = newNode(4);
-	T->left->left->right = newNode(30);
-	T->left->right	 = newNode(6);
-
-	// TREE 2
-	/* Construct the following tree
-		10
-		/ \
-	4 6
-	\
-		30
-	*/
-	node *S = newNode(10);
-	S->right	 = newNode(6);
-	S->left	 = newNode(4);
-	S->left->right = newNode(30);
-
-
-	if (isSubtree(T, S))
-		cout << "Tree 2 is subtree of Tree 1";
+	node *root = new node(4);
+	root->left = new node(2);
+	root->right = new node(5);
+	root->left->left = new node(1);
+	root->left->right = new node(3);
+	
+	if(isBST(root))
+		cout<<"Is BST";
 	else
-		cout << "Tree 2 is not a subtree of Tree 1";
-
+		cout<<"Not a BST";
+		
 	return 0;
 }
 
-// This code is contributed by rathbhupendra
